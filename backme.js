2f0e7392b23a1d79f3c72a537c5604ef2a8df10b
tejasukmana <dobolart@gmail.com>
2015-10-25 16:02:29 +0700

    Create backme.js

---
+++ backme.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#awas-ke-ciduk,.change").attr("href") != "http://mastamvan.blogspot.com/") {
+        window.location.href = "http://mastamvan.blogspot.com/"; }});
+//]]>
